72689a27f7131f90b554c340c1da947328c77345
Rj <yeeef25@gmail.com>
2024-03-03 20:25:04 -0500

    sending

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using Newtonsoft.Json;
+using RestTesting;
+using RestSharp.Authenticators;
+
+
+namespace Program
+{
+public class Program
+{
+    private static void Main()
+    {
+        var url = "https://www.boredapi.com/api/activity/";
+        var client = new RestClient(url);
+        var request = new RestRequest();
+        var response = client.Execute<Activities>(request);
+
+        if (response.IsSuccessful == true)//attempt connection
+            {
+            var activity = JsonConvert.DeserializeObject<Activities>(response.Content);//deserialize json into c# object 
+            Console.WriteLine($"Activity Recomendation:{activity.Activity}");
+            Console.WriteLine($"Requires: {activity.Participants} humans");
+            }
+        else
+        {
+            Console.WriteLine($"Connnection to server failed Error Code {response.StatusCode}"); 
+        }
+    }
+}
+
+}
